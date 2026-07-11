79ee34cf9bc5df597a312f0ef9a4b05165ef48c9
Daniel michael <michaellemakanit@gmail.com>
2026-07-11 03:13:24 +0300

    new commit

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ app = Flask(__name__)
 
 app.secret_key = 'a_secure_and_random_secret_key'
 
-@app.route('/')
-@app.route('/home')
-def home():
-    # This links the URL '/' to your HTML file
-    return render_template('index.html') 
-
-# dummy username and password for testing
+# # dummy username and password for testing
 USERNAME = "admin@example.com"
 PASSWORD = "password"
 
-# marchant/seller route
-@app.route('/seller/login', methods = ['GET', 'POST'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/reset_password')
+def reset_password():
+    return render_template('auth/reset_password.html')
+
+@app.route('/forgot_password')
+def forgot_password():
+    return render_template('auth/forgot_password.html')
+
+# # marchant/seller route
+@app.route('/seller/login', methods=['GET', 'POST'])
 def seller_login():
    if request.method == "POST":
         username = request.form['username']
@@ -27,28 +33,28 @@ def seller_login():
             return render_template('auth/seller/index.html', error='Invalid username or password')
    return render_template('auth/seller/login.html')
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+# @app.route('/dashboard')
+# def dashboard():
+#     return render_template('dashboard.html')
 
-# rider login route
-@app.route('/rider/login', methods = ['GET', 'POST'])
-def rider_login():
-    # if 'user_id' in session:
-    #     return redirect(url_for('home'))
-    # else:
-    #     return redirect(url_for('register.html'))
-    return render_template('auth/rider/login.html')
+# # rider login route
+# @app.route('/rider/login', methods = ['GET', 'POST'])
+# def rider_login():
+#     # if 'user_id' in session:
+#     #     return redirect(url_for('home'))
+#     # else:
+#     #     return redirect(url_for('register.html'))
+#     return render_template('auth/rider/login.html')
 
 
-# customers route
-@app.route('/customer/login', methods = ['GET', 'POST'])
-def customer_login():
-    # if 'user_id' in session:
-    #     return redirect(url_for('home'))
-    # else:
-    #     return redirect(url_for('register.html'))
-    return render_template('auth/customer/login.html', login='customer_login')
+# # customers route
+# @app.route('/customer/login', methods = ['GET', 'POST'])
+# def customer_login():
+#     # if 'user_id' in session:
+#     #     return redirect(url_for('home'))
+#     # else:
+#     #     return redirect(url_for('register.html'))
+#     return render_template('auth/customer/login.html', login='customer_login')
 
 
 
