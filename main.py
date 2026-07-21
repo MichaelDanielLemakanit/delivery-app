0e3a08c9ae7a9dbbbbd5cf49eff9d37f2b24a528
Daniel michael <michaellemakanit@gmail.com>
2026-07-21 03:04:12 +0300

    pages

--- main.py
+++ main.py
@@ -1,69 +1,30 @@
-from flask import Flask, render_template,redirect, request, url_for, session
+from flask import Flask, flash, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = '12345678'
 
-app.secret_key = 'a_secure_and_random_secret_key'
-
-# # dummy username and password for testing
-USERNAME = "admin@example.com"
-PASSWORD = "password"
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@app.route('/reset_password')
-def reset_password():
-    return render_template('auth/reset_password.html')
 
-@app.route('/forgot_password')
-def forgot_password():
-    return render_template('auth/forgot_password.html')
-
-# # marchant/seller route
-@app.route('/seller/login', methods=['GET', 'POST'])
-def seller_login():
-   if request.method == "POST":
-        username = request.form['username']
-        password = request.form['password']
-        if username == USERNAME and password == PASSWORD:
-            session['user_id'] = username
-            return redirect(url_for('dashboard'))
-        else:
-            return render_template('auth/seller/index.html', error='Invalid username or password')
-   return render_template('auth/seller/login.html')
-
-# @app.route('/dashboard')
-# def dashboard():
-#     return render_template('dashboard.html')
-
-# # rider login route
-# @app.route('/rider/login', methods = ['GET', 'POST'])
-# def rider_login():
-#     # if 'user_id' in session:
-#     #     return redirect(url_for('home'))
-#     # else:
-#     #     return redirect(url_for('register.html'))
-#     return render_template('auth/rider/login.html')
+#seller routes
+@app.route('/seller/home')
+def seller_home():
+    return render_template('seller/home.html')
 
 
-# # customers route
-# @app.route('/customer/login', methods = ['GET', 'POST'])
-# def customer_login():
-#     # if 'user_id' in session:
-#     #     return redirect(url_for('home'))
-#     # else:
-#     #     return redirect(url_for('register.html'))
-#     return render_template('auth/customer/login.html', login='customer_login')
+@app.route('/rider/dashboard')
+# @login_required
+def rider_dashboard():
+    return render_template('rider/dashboard.html')
+
+@app.route('/customer/dashboard')
+# @login_required
+def customer_dashboard():
+    return render_template('customer/dashboard.html')
 
 
 
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug = True)
