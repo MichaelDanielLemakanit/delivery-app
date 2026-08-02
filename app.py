85fb556e61f443ebc4f865326d4ce389de980197
Daniel michael <michaellemakanit@gmail.com>
2026-08-02 17:56:01 +0300

    deployment

--- app.py
+++ app.py
@@ -62,5 +62,6 @@ def rider_pages(page_name):
     abort(404)
                                                         
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+app = app
